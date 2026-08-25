bee04c70ef4565e998b1d0427c3a3269785d8884
mostakimjihad652001@gmail.com <mostakimjihad652001@gmail.com>
2026-08-26 01:38:35 +0600

    banner petty cash

--- hr_performance_kpi/__manifest__.py
+++ hr_performance_kpi/__manifest__.py
@@ -60,5 +60,5 @@ Features
     'application': True,
     'author': 'Mostakim Jihad',
     'license': 'LGPL-3',
-    'images': ['static/description/banner.gif'],
+    'images': ['static/description/banner.png'],
 }
